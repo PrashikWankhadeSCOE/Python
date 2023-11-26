039f33e8e5ad560768f0a0b8c813591204608b1a
PrashikWankhadeSCOE <prashikwankhade68@gmail.com>
2023-11-26 16:41:02 +0530

    String

---
+++ String/Program1.py
@@ -0,0 +1,13 @@
+# type 1 of writing string;
+str1 = 'Prashik'
+print(str1)
+#type 2 
+str2 = "Kabir"
+print(str2)
+#type 3 --> in """ only we can write anything in nextline in above two types we cannot write like this 
+str3 = """Biencaps
+systems
+pvt 
+ltd"""
+print(str3)
+
---
+++ String/Program3.py
@@ -0,0 +1,12 @@
+str1 = 'Prashik'
+print(str1[3])
+print(str1[6])
+print(str1[-2])
+print(str1[-5])
+
+print(str1[::2])
+print(str1[2:5:])
+print(str1[-2:-5:-1])
+print(str1[2:5:-1])
+
+
