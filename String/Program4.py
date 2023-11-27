33c67e2c2cb1cbbaa51f30858cc116fed03d9451
PrashikWankhadeSCOE <prashikwankhade68@gmail.com>
2023-11-27 22:31:05 +0530

    String methods

---
+++ String/Program4.py
@@ -0,0 +1,19 @@
+# Methods in String 
+
+str1 = 'core2web incubator'
+
+print(str1)
+# core2web incubator
+
+str2 = 'core2web Incubator'
+print(str1==str2)
+print(str1.casefold() == str2.casefold())
+
+print(str1.expandtabs(4))
+
+"""
+core2web incubator
+False
+True
+core2web incubator
+"""
---
+++ String/Program5.py
@@ -0,0 +1,9 @@
+# Check if str2 is present in str1 
+
+str1 = input()
+str2 = input()
+
+if str2 in str1:
+    print("Found")
+else :
+    print("Not found")
