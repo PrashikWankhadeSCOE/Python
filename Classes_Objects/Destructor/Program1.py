405d7fb950bad24ee18d4423dd1a87c9bbe3689f
PrashikWankhadeSCOE <prashikwankhade68@gmail.com>
2023-12-30 23:31:27 +0530

    Destructor

---
+++ Classes_Objects/Destructor/Program1.py
@@ -0,0 +1,23 @@
+class Parent:
+    z = 30
+    def __init__(self):
+        print('In Parent constructor')
+        self.x = 10
+        self.y = 20
+
+    def dispData(self):
+        print(self.x)
+        print(self.y)
+
+    @classmethod
+    def dispParent(cls):
+        print(cls.z)
+
+    def __del__(self):
+        print('In destructor')
+
+obj = Parent()
+obj.dispData()
+obj.dispParent()
+del obj
+
